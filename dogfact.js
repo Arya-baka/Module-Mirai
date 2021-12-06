06295cac49a9f8db9ddb0cfb2e361a4defd72ae4
Yotsuba-so-cute <92246276+Yotsuba-so-cute@users.noreply.github.com>
2021-12-06 10:42:53 +0700

    Update dogfact.js

--- dogfact.js
+++ dogfact.js
@@ -9,13 +9,26 @@ module.exports.config = {
     cooldowns: 0,
 };
 
-module.exports.run = async function({ api, event, args, Currencies, utils,Users,Threads }) {
-	let axios = require('axios')
-	let { threadID, senderID, messageID } = event;
-	const res = await axios.get("https://some-random-api.ml/facts/dog");
+module.exports.run = async function({api, event, args, utils, Users, Threads}) {
+    try {
+        let axios = require('axios');
+        let fs = require("fs-extra");
+        let request = require("request");
+        let {threadID, senderID, messageID} = event;
+	const res = await axios.get(`https://some-random-api.ml/animal/dog`);
 	console.log(res.data);
 	var data = res.data;
 	const translate = await axios.get(encodeURI(`https://lawerpr0ject.herokuapp.com/other/translate?text=${data.fact}`));
 	var fact = translate.data.trans;
-	return api.sendMessage(`===🐶Dog fact🐶===\n\nFact: ${fact}`, threadID, messageID)
+	let callback = function() {
+            return api.sendMessage({
+                body:`===🐶Dog fact🐶===\n\nFact: ${fact}`,
+                attachment: fs.createReadStream(__dirname + `/cache/image.png`)
+            }, event.threadID, () => fs.unlinkSync(__dirname + `/cache/image.png`), event.messageID);
+        };
+		return request(encodeURI(data.image)).pipe(fs.createWriteStream(__dirname + `/cache/image.png`)).on("close", callback);
+		} catch (err) {
+        console.log(err)
+        return api.sendMessage(`Đã xảy ra lỗi`, event.threadID)
+    }
 }
