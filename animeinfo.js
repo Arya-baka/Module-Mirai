2ab9f513e6cecdb13e0f0f2873342088f7797fe2
Nguyễn Minh <92246276+Yotsuba-so-cute@users.noreply.github.com>
2021-12-07 10:26:01 +0700

    Update animeinfo.js

--- animeinfo.js
+++ animeinfo.js
@@ -32,11 +32,9 @@ module.exports.run = async function({
         const res = await axios.get(encodeURI(`http://lawerpr0ject.herokuapp.com/other/anime?name=${args[0]}`));
         console.log(res.data);
         let data = res.data;
-        const translate = await axios.get(encodeURI(`https://lawerpr0ject.herokuapp.com/other/translate?text=${data.noidung}&to=vi`));
-        var noidung = translate.data.trans;
         let callback = function() {
             return api.sendMessage({
-                body: `Tên phim: ${data.title}\nurl: ${data.url}\nNội dung phim: ${noidung}\nXếp hạng: 🏆${data.xephang}\nSố tập: ${data.episodes}\nAuthor: ${data.Author}`,
+                body: `Tên phim: ${data.title}\nurl: ${data.url}\nNội dung phim: ${data.noidung}\nXếp hạng: 🏆${data.xephang}\nSố tập: ${data.episodes}\nAuthor: ${data.Author}`,
                 attachment: fs.createReadStream(__dirname + `/cache/anime.png`)
             }, event.threadID, () => fs.unlinkSync(__dirname + `/cache/anime.png`), event.messageID);
         };
