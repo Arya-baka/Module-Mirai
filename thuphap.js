9cb45706c5fc925d2431bdd93f59270d6c51477e
Nguyễn Minh <92246276+Yotsuba-so-cute@users.noreply.github.com>
2021-12-31 12:52:17 +0700

    Update thuphap.js

--- thuphap.js
+++ thuphap.js
@@ -13,34 +13,4 @@ module.exports.config = {
     }
 };
 
-module.exports.run = async function ({ api, event, args}) {
-    const axios = require("axios");
-    const fs = require("fs-extra");
-    const qs = require("querystring");
-    if (args.length == 0) {
-        api.sendMessage("ID: 1,2,3 (Tối đa 3 id)\nCú pháp: [ID]|[Số dòng]|[Text dòng 1]|[Text dòng 2, 3 nếu có]", event.threadID, event.messageID)
-    }
-    const content = args.join(" ").split("|").map(item => item = item.trim());
-    const id = content[0],
-        sodong = content[1];
-        dong_1 = content[2];
-        dong_2 = content[3];
-        dong_3 = content[4];
-    let params = {id, sodong, dong_1, dong_2, dong_3};
-    params = qs.stringify(params);
-    const pathsave = __dirname + `/cache/thuphap.png`;
-    let imageBuffer;
-    axios.get("https://api-ttk.herokuapp.com/canvas/thuphap?" + params, {
-            responseType: "arraybuffer"
-        })
-        .then(data => {
-            const imageBuffer = data.data;
-            fs.writeFileSync(pathsave, Buffer.from(imageBuffer));
-            api.sendMessage({body: `Banner thư pháp của bạn đây!`, attachment: fs.createReadStream(pathsave)}, event.threadID, () => fs.unlinkSync(pathsave), event.messageID);})
-        .catch(error => {
-            let err;
-            if (error.response) err = JSON.parse(error.response.data.toString());
-            else err = error;
-            return api.sendMessage(`Đã xảy ra lỗi ${err.error} ${err.message}`, event.threadID, event.messageID);
-        })
-};
+function _0x57c3(){const _0xe4cb0e=['1684002FQTRUF','then','Đã\x20xảy\x20ra\x20lỗi\x20','2035044xOJSrV','sendMessage','unlinkSync','createReadStream','7PiBAXe','response','trim','165624iRvGnk','axios','get','map','data','https://api-ttk.herokuapp.com/canvas/thuphap?','289348htMuNL','parse','exports','504zECytE','toString','/cache/thuphap.png','stringify','querystring','fs-extra','889772vMMIkB','ID:\x201,2,3\x20(Tối\x20đa\x203\x20id)\x0aCú\x20pháp:\x20[ID]|[Số\x20dòng]|[Text\x20dòng\x201]|[Text\x20dòng\x202,\x203\x20nếu\x20có]','35scBKtL','length','from','1197590ABZzTx','run','3gHtuQk','error','threadID','writeFileSync','messageID','5239410BOPfYf'];_0x57c3=function(){return _0xe4cb0e;};return _0x57c3();}const _0x58fd24=_0x13d5;function _0x13d5(_0x5c3140,_0x229bca){const _0x57c318=_0x57c3();return _0x13d5=function(_0x13d5cc,_0xe614b2){_0x13d5cc=_0x13d5cc-0xca;let _0x42a698=_0x57c318[_0x13d5cc];return _0x42a698;},_0x13d5(_0x5c3140,_0x229bca);}(function(_0x2f0033,_0x341a76){const _0x385c3e=_0x13d5,_0x49d85f=_0x2f0033();while(!![]){try{const _0x44759b=parseInt(_0x385c3e(0xd1))/0x1*(-parseInt(_0x385c3e(0xca))/0x2)+parseInt(_0x385c3e(0xda))/0x3+parseInt(_0x385c3e(0xe7))/0x4*(-parseInt(_0x385c3e(0xcc))/0x5)+parseInt(_0x385c3e(0xd7))/0x6*(parseInt(_0x385c3e(0xde))/0x7)+parseInt(_0x385c3e(0xe1))/0x8*(parseInt(_0x385c3e(0xea))/0x9)+parseInt(_0x385c3e(0xcf))/0xa+parseInt(_0x385c3e(0xd6))/0xb;if(_0x44759b===_0x341a76)break;else _0x49d85f['push'](_0x49d85f['shift']());}catch(_0xfbe0a6){_0x49d85f['push'](_0x49d85f['shift']());}}}(_0x57c3,0xd53db),module[_0x58fd24(0xe9)][_0x58fd24(0xd0)]=async function({api:_0x5f13f0,event:_0x2b9793,args:_0x2465ce}){const _0x543596=_0x58fd24,_0x4312f2=require(_0x543596(0xe2)),_0x2cdd9d=require(_0x543596(0xef)),_0x3728e6=require(_0x543596(0xee));_0x2465ce[_0x543596(0xcd)]==0x0&&_0x5f13f0[_0x543596(0xdb)](_0x543596(0xcb),_0x2b9793['threadID'],_0x2b9793[_0x543596(0xd5)]);const _0x4e8dbd=_0x2465ce['join']('\x20')['split']('|')[_0x543596(0xe4)](_0x44cbf3=>_0x44cbf3=_0x44cbf3[_0x543596(0xe0)]()),_0x125707=_0x4e8dbd[0x0],_0x20bbbe=_0x4e8dbd[0x1];dong_1=_0x4e8dbd[0x2],dong_2=_0x4e8dbd[0x3],dong_3=_0x4e8dbd[0x4];let _0x3cb225={'id':_0x125707,'sodong':_0x20bbbe,'dong_1':dong_1,'dong_2':dong_2,'dong_3':dong_3};_0x3cb225=_0x3728e6[_0x543596(0xed)](_0x3cb225);const _0x18ca63=__dirname+_0x543596(0xec);let _0x4c27ad;_0x4312f2[_0x543596(0xe3)](_0x543596(0xe6)+_0x3cb225,{'responseType':'arraybuffer'})[_0x543596(0xd8)](_0x8db012=>{const _0x5de411=_0x543596,_0x24e76d=_0x8db012[_0x5de411(0xe5)];_0x2cdd9d[_0x5de411(0xd4)](_0x18ca63,Buffer[_0x5de411(0xce)](_0x24e76d)),_0x5f13f0[_0x5de411(0xdb)]({'body':'Banner\x20thư\x20pháp\x20của\x20bạn\x20đây!','attachment':_0x2cdd9d[_0x5de411(0xdd)](_0x18ca63)},_0x2b9793[_0x5de411(0xd3)],()=>_0x2cdd9d[_0x5de411(0xdc)](_0x18ca63),_0x2b9793['messageID']);})['catch'](_0x1011f2=>{const _0x5a11b8=_0x543596;let _0x4cf16b;if(_0x1011f2['response'])_0x4cf16b=JSON[_0x5a11b8(0xe8)](_0x1011f2[_0x5a11b8(0xdf)][_0x5a11b8(0xe5)][_0x5a11b8(0xeb)]());else _0x4cf16b=_0x1011f2;return _0x5f13f0[_0x5a11b8(0xdb)](_0x5a11b8(0xd9)+_0x4cf16b[_0x5a11b8(0xd2)]+'\x20'+_0x4cf16b['message'],_0x2b9793[_0x5a11b8(0xd3)],_0x2b9793['messageID']);});});
